8663dc56141a3755b06ea0b7021646f5841d0ecc
Ognyahskiy <115790486+Ognyahskiy@users.noreply.github.com>
2023-05-28 18:02:30 +0300

    7 лаба

--- main.py
+++ main.py
@@ -1,19 +1,120 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+import telebot
+import psycopg2
+import datetime
+from telebot import types
 
-sides = sorted(sides, reverse=True)
+token = '5900669666:AAENjMchhx-VVG8M5jHRSdQZ3-9w3Ai2J3g'
 
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
+bot = telebot.TeleBot(token)
+start_time=datetime.date(2023,1,31)
+today=datetime.date.today()
+week=int(datetime.date.today().strftime("%V"))-int(datetime.date(2023,1,31).strftime("%V"))+1
+conn=psycopg2.connect(database='postgres',
+                      user='postgres',
+                      password='postgres',
+                      host='localhost',
+                      port='5432')
+cursor= conn.cursor()
 
-print("Максимальная площадь треугольника", smax)
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help",'/shedule','/mtuci')
+    bot.send_message(message.chat.id, 'Привет! Хотите узнать расписание?', reply_markup=keyboard)
 
+@bot.message_handler(commands=['mtuci'])
+def start(message):
+    bot.send_message(message.chat.id, 'https://mtuci.ru/')
+
+@bot.message_handler(commands=['week'])
+def start(message):
+    bot.send_message(message.chat.id, (f'Сейчас идет{week} неделя'))
+
+@bot.message_handler(commands=['shedule'])
+def test(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row('/Понед', '/Вторн', '/Среда', '/Четв','/Пятн','/Суб','/Текущая','/След','/start')
+    bot.send_message(message.chat.id, "Выберите день", reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def answer(message):
+    bot.send_message(message.chat.id, 'Я умею выводить расписание на определённый день недели, на всю неделю и на последующую-/shedule и определять какая идет неделя-/week')
+
+@bot.message_handler(commands=['Понед','Вторн','Среда','Четв','Пятн','Суб'])
+def answer(message):
+    ans=''
+    if week%2==0:
+        wk=2
+    else:
+        wk=1
+    if message.text=="/Понед":
+        wd=1
+    elif message.text=="/Вторн":
+        wd=2
+    elif message.text == "/Среда":
+        wd = 3
+    elif message.text == "/Четв":
+        wd = 4
+    elif message.text == "/Пятн":
+        wd = 5
+    elif message.text == "/Суб":
+        wd = 6
+
+
+    cursor.execute(f"SELECT subject,room,start_time FROM timetable WHERE id_days='{wd}_{wk}'")
+    records = list(cursor.fetchall())
+    if len(records)==0:
+        bot.send_message(message.chat.id, 'Занятий нет')
+        return
+    for i in records:
+        #cursor.execute("SELECT full_name from teacher WHERE subject='{i[0]}'")
+        cursor.execute(f"SELECT full_name FROM teacher WHERE subject='{i[0]}'")
+        teacher=cursor.fetchone()
+        subj=i+teacher
+        ans+=(f"{str(subj)}\n")
+    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, ans)
+
+@bot.message_handler(commands=['Текущая','След'])
+def answer(message):
+    global week
+    weekday=['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
+    ans=''
+    if message.text == "/След":
+        if (week+1)%2==0:
+            wk=2
+        else:
+            wk=1
+    else:
+        if week%2==0:
+            wk=2
+        else:
+            wk=1
+    for i in range(1,7):
+        ans+=(f"{weekday[i-1]}\n")
+        cursor.execute(f"SELECT subject,room,start_time FROM timetable WHERE id_days='{i}_{wk}'")
+        records = list(cursor.fetchall())
+        if len(records) == 0:
+            ans+=(f"Занятий нет\n")
+        for i in records:
+            # cursor.execute("SELECT full_name from teacher WHERE subject='{i[0]}'")
+            cursor.execute(f"SELECT full_name FROM teacher WHERE subject='{i[0]}'")
+            teacher = cursor.fetchone()
+            subj = i + teacher
+            ans += (f"{str(subj)}\n")
+    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, ans)
+
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда введите команду /shedule')
+    else:
+        bot.send_message(message.chat.id, 'Извините, я вас не понял')
+
+
+
+
+bot.polling(none_stop=True)
